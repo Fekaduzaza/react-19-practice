6ba5dc9f653cc910bec688f18c0020417e17e656
Fekadu zaza <fkdzaza@gmail.com>
2024-11-24 17:42:15 +0300

    react practice

---
+++ MainContent.jsx
@@ -0,0 +1,11 @@
+export default function MainContent(){
+ return(
+    <main>
+        <h1>Reasons I'am excited to learn React!</h1>
+        <ol>
+            <li>React</li>
+            <li>Javascript</li>
+        </ol>
+        </main>
+ )
+}
---
+++ components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return(
+   <main>
+      <h1>Fun facts about React!</h1>
+      <ul className="facts-list">
+        <li>was first released in 2013</li>
+        <li>was originally created by Jordan Walke</li>
+        <li>Has well over 200k stars on Github</li>
+        <li>Is maintained by meta</li>
+        <li>Powers thousands of enterprise apps,including mobile apps.</li>
+      </ul>
+   </main>
+  )
+}
